1792e49c376495b684331f9a16188229d22f92ea
AlexMaddox3 <42100657+AlexMaddox3@users.noreply.github.com>
2019-02-06 12:13:12 -0500

    Update RandomWordManagement.java

--- JavaGroupHangman/src/GE/RandomWordManagement.java
+++ JavaGroupHangman/src/GE/RandomWordManagement.java
@@ -1,5 +1,14 @@
 package GE;
 
 public class RandomWordManagement {
+import java.util.Random;
 
+    public static void getRandomWord() 
+        {
+
+        String[] word = {"Artichoke", "Bearskin", "Cuttlefish", "Digital", "Earwax", "Fluter", "Goatsbeard", "Huckleberry", "Imposter", "Juniper", "Knobbly", "Lantern", "Mammoth", "Nightmare", "Origami", "Playwright", "Quartzrose", "Racoon", "Sorcerer", "Tablespoon", "Ultrasound", "Vessel", "Yesterday", "Zesty"};
+
+        System.out.println( word[new Random().nextInt(word.length)]);   
+
+        }
 }
