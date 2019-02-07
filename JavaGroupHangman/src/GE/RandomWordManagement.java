ec6da6486637981e3d0795460f87f786748cf216
AlexMaddox3 <42100657+AlexMaddox3@users.noreply.github.com>
2019-02-07 17:41:16 -0500

    RachelsRandomWordsTry2
    Updated with string variables.

--- JavaGroupHangman/src/GE/RandomWordManagement.java
+++ JavaGroupHangman/src/GE/RandomWordManagement.java
@@ -1,5 +1,10 @@
 package GE;
 
-public class RandomWordManagement {
+  public static String getRandomWord() 
+    {
+        String[] word = {"Artichoke", "Bearskin", "Cuttlefish", "Digital", "Earwax", "Fluter", "Goatsbeard", "Huckleberry", "Imposter", "Juniper", "Knobbly", "Lantern", "Mammoth", "Nightmare", "Origami", "Playwright", "Quartzrose", "Racoon", "Sorcerer", "Tablespoon", "Ultrasound", "Vessel", "Yesterday", "Zesty"};
+        Random rand = new Random();
+        String randWord = new String(word[rand.nextInt(word.length)]);   
+        return randWord;
 
-}
+    }
